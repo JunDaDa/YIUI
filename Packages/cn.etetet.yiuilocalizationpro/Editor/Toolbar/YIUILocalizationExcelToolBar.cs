5011aaed4374329e02049535757556a8d95cd82c
sunjun <271830912@qq.com>
2026-03-25 02:26:33 +0800

    多语言插件I2Localization

---
+++ Packages/cn.etetet.yiuilocalizationpro/Editor/Toolbar/YIUILocalizationExcelToolBar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace YIUIFramework.Editor
+{
+    [InitializeOnLoad]
+    public static class YIUILocalizationExcelToolBar
+    {
+        private static Texture _cachedIcon;
+
+        static YIUILocalizationExcelToolBar()
+        {
+            YIUIToolbarExtender.AddLeftToolbarGUI(OnLubanExcelToolbarGUI, 1000);
+        }
+
+        private static void OnLubanExcelToolbarGUI()
+        {
+            _cachedIcon ??= AssetDatabase.LoadAssetAtPath<Texture>("Packages/cn.etetet.yiuilocalizationpro/Editor/Toolbar/Icon/icon_localization_chn.png");
+            GUILayout.Space(5);
+            GUIContent iconContent = new(string.Empty, _cachedIcon);
+            iconContent.tooltip = "多语言导出";
+            if (GUILayout.Button(iconContent))
+            {
+                YIUILocalizationCreate.CreateI2LocalizationByXlsx();
+            }
+
+            GUILayout.Space(5);
+        }
+    }
+}
